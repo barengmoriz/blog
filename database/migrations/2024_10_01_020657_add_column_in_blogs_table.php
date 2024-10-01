56cc2bdcc0845639169d26785dfb0712d0cc68eb
barengmoriz <barengmoriz@gmail.com>
2024-10-01 10:04:50 +0700

    Add column short_description and is_publish

---
+++ database/migrations/2024_10_01_020657_add_column_in_blogs_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->text('short_description')->after('image')->nullable();
+            $table->boolean('is_publish')->after('user_id')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->dropColumn(['short_description', 'is_publish']);
+        });
+    }
+};
