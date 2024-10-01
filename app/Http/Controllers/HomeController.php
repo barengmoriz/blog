f651a18499e865f0012d530fedcbbda8ce50b89f
barengmoriz <barengmoriz@gmail.com>
2024-10-01 10:08:21 +0700

    Show blog in public just published blog

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        $blogs = Blog::latest()->paginate(10);
+        $blogs = Blog::latest()->where('is_publish', true)->paginate(10);
         return view('public.home', [
             'blogs' => $blogs 
         ]);
