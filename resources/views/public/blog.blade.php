cac0548e1cf5dfe7fdf2bd6e0e7cfe191f1af597
barengmoriz <barengmoriz@gmail.com>
2024-10-05 00:36:25 +0700

    Add image in name

--- resources/views/public/blog.blade.php
+++ resources/views/public/blog.blade.php
@@ -7,7 +7,10 @@
             <img src="{{ Storage::url($blog->image) }}" alt="" class="object-cover object-center w-full rounded h-fit">
         </div>
         <div class="flex justify-between">
-            <div>{{ $blog->user->name }}</div>
+            <div class="flex items-center space-x-2">
+                <img class="object-cover rounded-full size-8" src="{{ $blog->user->image ? Storage::url($blog->user->image) : Avatar::create($blog->user->name)->toBase64() }}" alt="">
+                <div>{{ $blog->user->name }}</div>
+            </div>
             <div>{{ $blog->custom_created_at }}</div>
         </div>
         <div class="flex justify-between">
