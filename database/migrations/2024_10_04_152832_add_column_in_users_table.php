34513811c7e9811045126e770c2aeb766016d3d7
barengmoriz <barengmoriz@gmail.com>
2024-10-05 00:26:15 +0700

    Add migration

---
+++ database/migrations/2024_10_04_152832_add_column_in_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('username')->unique()->nullable()->after('name');
+            $table->string('image')->nullable()->after('email');
+            $table->boolean('is_active')->default(true)->after('email_verified_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['username', 'image', 'is_active']);
+        });
+    }
+};
