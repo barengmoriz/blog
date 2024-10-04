11740a5a498092ae3f21a2b0a4bea9016bd5d6bb
barengmoriz <barengmoriz@gmail.com>
2024-10-05 00:28:35 +0700

    Add route for image profile

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::put('/profile/image', [ProfileController::class, 'imageUpload'])->name('profile.image.upload');
+    Route::delete('/profile/image', [ProfileController::class, 'imageDestroy'])->name('profile.image.destroy');
 });
 
 require __DIR__.'/auth.php';
