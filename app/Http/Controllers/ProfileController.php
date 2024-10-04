6adb4cfcac1ce399007990c0f6f9f09f2dd9c43e
barengmoriz <barengmoriz@gmail.com>
2024-10-05 00:31:12 +0700

    Add function for upload and remove image

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\View\View;
 
 class ProfileController extends Controller
@@ -32,9 +33,20 @@ class ProfileController extends Controller
             $request->user()->email_verified_at = null;
         }
 
+        if ($request->user()->isDirty('username') && auth()->user()->image) {
+            $extension = pathinfo(storage_path(auth()->user()->image), PATHINFO_EXTENSION);
+            $fileName = 'images/user/' . $request->user()->username . '.' . $extension;
+
+            Storage::move(auth()->user()->image, $fileName);
+
+            auth()->user()->update([
+                'image' => $fileName
+            ]);
+        }
+
         $request->user()->save();
 
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+        return Redirect::route('profile.edit')->with('success', 'Successfully update profile');
     }
 
     /**
@@ -56,5 +68,40 @@ class ProfileController extends Controller
         $request->session()->regenerateToken();
 
         return Redirect::to('/');
+    }
+
+    public function imageUpload(Request $request){
+        $request->validate([
+            'image' => ['required', 'image', 'mimes:jpg,jpeg,png']
+        ]);
+
+        if(auth()->user()->image){
+            Storage::delete(auth()->user()->image);
+        }
+
+        $file = $request->file('image');
+        $image = $file->storeAs('images/user', auth()->user()->username . '.' . $file->extension());
+
+        auth()->user()->update([
+            'image' => $image
+        ]);
+
+        return Redirect::route('profile.edit')->with('success', 'Successfully upload image');
+    }
+
+    public function imageDestroy(){
+        if(auth()->user()->image){
+            Storage::delete(auth()->user()->image);
+        }
+
+        auth()->user()->update([
+            'image' => null
+        ]);
+
+
+        return [
+            'success' => true,
+            'message' => 'Gambar Berhasil Dihapus'
+        ];
     }
 }
